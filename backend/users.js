285394dd257c36f4ba6ca4fcf0793a86d766e345
richardalbani <richardalbani2@gmail.com>
2025-06-04 09:23:02 -0300

    Update users.js

--- backend/users.js
+++ backend/users.js
@@ -1,5 +1,5 @@
 module.exports = [
   { id: 1, username: "admin", password: "admin123", role: "admin" },
   { id: 2, username: "user", password: "user123", role: "user" }
-  { id: 2, username: "richard", password: "richardcruz", role: "user" }
+  { id: 3, username: "richard", password: "richardcruz", role: "user" }
 ];
